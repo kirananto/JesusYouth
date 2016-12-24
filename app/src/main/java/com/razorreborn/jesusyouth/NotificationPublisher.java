749afbdf930c5bfee2b82f3c1a4713c5a71fdc8d
razorSharp <kirananto@gmail.com>
2016-12-24 09:20:41 +0530

    Make variables final

--- app/src/main/java/com/razorreborn/jesusyouth/NotificationPublisher.java
+++ app/src/main/java/com/razorreborn/jesusyouth/NotificationPublisher.java
@@ -12,8 +12,8 @@ import android.content.Intent;
 
 public class NotificationPublisher extends BroadcastReceiver {
 
-    public static String NOTIFICATION_ID = "notification-id";
-    public static String NOTIFICATION = "notification";
+    public static final String NOTIFICATION_ID = "notification-id";
+    public static final String NOTIFICATION = "notification";
 
     public void onReceive(Context context, Intent intent) {
 
